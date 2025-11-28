2a2ce7400734db9c9ec7d22595443d6e8405c1e5
Paul Norman <penorman@mac.com>
2025-11-27 19:56:15 -0800

    Publish a version of the style with non-relative URLs
    This avoids the need to mangle URLs and makes it easier for
    sites like the wiki to add vector tiles.
    
    The old /demo paths are kept working to avoid breaking the
    website before it points at the new URLs.
    
    The style file being published is currently only intended for
    OSM use so it has CORS headers only set for OSM domains and
    127.0.0.1.
    
    The demo pages have been partially moved over to the new
    paths but not completely. That will be done after the /styles/
    paths are deployed on the OSMF servers.

--- build-styles.ts
+++ build-styles.ts
@@ -3,14 +3,26 @@ import { writeFileSync, mkdirSync } from 'node:fs';
 import { colorful, eclipse, graybeard, neutrino, shadow } from "@versatiles/style";
 import { StyleSpecification, validateStyleMin } from '@maplibre/maplibre-gl-style-spec';
 
-const saveLocation = "_versatiles/shortbread";
-const styleOptions = {
+const demoSaveLocation = "_versatiles/shortbread";
+const demoStyleOptions = {
     language: undefined,
     baseUrl: '',
     tiles: ['/shortbread_v1/{z}/{x}/{y}.mvt'],
     glyphs: '/demo/shortbread/fonts/{fontstack}/{range}.pbf',
     sprite: [{ id: 'basics', url: '/demo/shortbread/sprites/basics/sprites' }]
 }
+
+const saveLocation = "release/shortbread";
+const styleOptions = {
+    language: undefined,
+    baseUrl: 'https://vector.openstreetmap.org',
+    tiles: ['/shortbread_v1/{z}/{x}/{y}.mvt'],
+    glyphs: '/demo/shortbread/fonts/{fontstack}/{range}.pbf',
+    sprite: [{ id: 'basics', url: '/demo/shortbread/sprites/basics/sprites' }]
+}
+
+const demoDirDst = new URL(demoSaveLocation, import.meta.url).pathname;
+mkdirSync(demoDirDst, { recursive: true });
 
 const dirDst = new URL(saveLocation, import.meta.url).pathname;
 mkdirSync(dirDst, { recursive: true });
@@ -22,9 +34,21 @@ mkdirSync(dirDst, { recursive: true });
 	{ name: 'neutrino', builder: neutrino },
 	{ name: 'shadow', builder: shadow },
 ].forEach(({ name, builder }) => {
+	demoProduce(name, builder(demoStyleOptions));
 	produce(name, builder(styleOptions));
 });
 
+
+function demoProduce(name: string, style: StyleSpecification): void {
+
+	// Validate the style and log errors if any
+	const errors = validateStyleMin(style);
+	if (errors.length > 0) console.log(errors);
+
+	// write
+    writeFileSync(demoSaveLocation + '/' + name + '.json', JSON.stringify(style, null, 2));
+	console.log('Saved ' + name);
+}
 
 function produce(name: string, style: StyleSpecification): void {
 
